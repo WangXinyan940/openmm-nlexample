df8157217d323756675a97cdf12335b32791e6d0
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-01-29 16:36:28 +0800

    Update noPBCForce.cu

--- platforms/cuda/src/kernels/noPBCForce.cu
+++ platforms/cuda/src/kernels/noPBCForce.cu
@@ -5,18 +5,17 @@ extern "C" __global__ void calcTestForceNoPBC(
     int*                __restrict__     pairidx0,
     int*                __restrict__     pairidx1,
     int                                  numParticles,
-    int                                  paddedNumAtoms
-) {
+    int                                  paddedNumAtoms) {
     int totpair = numParticles * (numParticles - 1) / 2;
     for (int npair = blockIdx.x*blockDim.x+threadIdx.x; npair < totpair; npair += blockDim.x*gridDim.x) {
         int ii = pairidx0[npair];
         int jj = pairidx1[npair];
-        real4 delta(posq[jj].x-posq[ii].x,posq[jj].y-posq[ii].y,posq[jj].z-posq[ii].z,0);
+        real3 delta = make_real3(posq[jj].x-posq[ii].x,posq[jj].y-posq[ii].y,posq[jj].z-posq[ii].z);
         real R2 = delta.x * delta.x + delta.y * delta.y + delta.z * delta.z;
         real inverseR = RSQRT(R2);
         energyBuffer[npair] += 100.0 * inverseR * inverseR;
         real dEdR = - 200.0 * inverseR * inverseR * inverseR;
-        real4 force = dEdR * delta * inverseR;
+        real3 force = dEdR * delta * inverseR;
         atomicAdd(&forceBuffers[ii], static_cast<unsigned long long>((long long) (-force.x*0x100000000)));
         atomicAdd(&forceBuffers[ii+paddedNumAtoms], static_cast<unsigned long long>((long long) (-force.y*0x100000000)));
         atomicAdd(&forceBuffers[ii+2*paddedNumAtoms], static_cast<unsigned long long>((long long) (-force.z*0x100000000)));
